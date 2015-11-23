bd4547a7080375d071674e8a90c2d658724c1e72
Nivedita Ramakrishnan <animelikr@gmail.com>
2015-11-23 15:35:39 +0530

    Jik

---
+++ Program3Part1.java
@@ -0,0 +1,14 @@
+
+public class Program3Part1 {
+
+	public static void main(String[] args) {
+		
+		//examples for data that can't be stored in a variable of int dataype
+	
+	int example1 = 55.67, example2 = -210000;
+	
+	  System.out.println("Int does not support this: "+example1+ " and " +example2);
+	
+	}
+
+}
---
+++ Program4Part1.java
@@ -0,0 +1,21 @@
+
+public class Program4Part1 {
+
+	public static void main(String[] args) {
+		
+		int x = 5, y = 10, a, b, c;
+		//int d;
+		float d;
+	
+		   a = x + y * 2;
+		   
+		   b = x - y + 2;
+		   
+		   c = (x+y) * 2;
+		   
+		   d = y % x;
+		   
+System.out.println("The values of the variables a, b, c and d are: "+a+ "\n" +b+ "\n" +c+ "\n" +d);
+	}
+
+}
